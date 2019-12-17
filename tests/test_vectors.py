4fe65077cd7f694d1d58b3215206ebca0fee3f21
Branden Akana <branden.akana@gmail.com>
2019-12-17 00:24:45 -1000

    create tests folder

---
+++ tests/test_vectors.py
@@ -0,0 +1,18 @@
+
+from structs.vector import Vector
+
+vec = Vector(0, 0)
+
+
+def test_comparisons():
+
+    assert vec == (0, 0)
+    assert vec == Vector(0, 0)
+
+
+def test_operations():
+
+    assert vec + (1, 1) == (1, 1)
+    assert vec + Vector(1, 1) == (1, 1)
+
+    assert Vector(1, 1) * 2 == (2, 2)
