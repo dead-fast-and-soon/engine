a7b272ddc4fff91b0a09afe856a21358f848104e
Branden Akana <branden.akana@gmail.com>
2020-01-09 07:13:21 -1000

    add type hinting for image assets

--- engine/asset/image.py
+++ engine/asset/image.py
@@ -2,7 +2,7 @@
 Contains image-related assets.
 """
 
-import pyglet
+from pyglet.image import AbstractImage, TileableTexture, load
 
 
 class ImageAsset():
@@ -16,9 +16,25 @@ class ImageAsset():
         Args:
             path_or_image (str): the path to the image
         """
+        self.pyglet_image: AbstractImage = None
         if type(path_or_image) is str:
-            self.pyglet_image = pyglet.image.load(path_or_image)
-        elif isinstance(path_or_image, pyglet.image.AbstractImage):
+            self.pyglet_image = load(path_or_image)
+        elif isinstance(path_or_image, AbstractImage):
             self.pyglet_image = path_or_image
         else:
             raise ValueError('image must be a path or a pyglet image')
+
+
+class TileableImageAsset():
+    """
+    A wrapper class for images loaded using Pyglet.
+    """
+    def __init__(self, image_asset: ImageAsset):
+        """
+        Loads an image asset.
+
+        Args:
+            path_or_image (str): the path to the image
+        """
+        self.pyglet_image = (TileableTexture
+                             .create_for_image(image_asset.pyglet_image))
